842cc9fd3b471a2bf06f0110846ac5b4efa64afe
KitaTooru <1744086723@qq.com>
2024-12-27 10:26:28 +0800

    update

--- graph_2.5_K.cpp
+++ graph_2.5_K.cpp
@@ -1,56 +1,91 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
-#define vNum 100
-#define eNum 1000
+#define vNum 6
+#define eNum 15
 
 struct edge
 {
-    int v;
-    int w;
-    edge(int v1 = 0, int w1 = 0) : v(v1), w(w1) {}
+    int u, v, w;
+    edge(int u1 = 0, int v1 = 0, int w1 = 0) : u(u1), v(v1), w(w1) {}
+    bool operator<(const edge &e) const
+    {
+        return w < e.w;
+    }
 };
 
-struct node
-{
-    int u, v, w;
-    node(int u1 = 0, int v1 = 0, int w1 = 0) : u(u1), v(v1), w(w1) {}
-    bool operator<(node n1) const
-    {
-        return w < n1.w;
-    }
-} edges[eNum];
+vector<edge> edges;
+vector<int> parent(vNum + 1, -1);
+vector<int> t[vNum + 1];
 
-void kruskal(vector<edge> g[vNum], int n, vector<int> t[vNum])
+int find(int x)
 {
-    int i, j, cnt = 0, num = 0;
-    for (i = 1; i <= n; i++)
+    if (parent[x] < 0)
+        return x;
+    return parent[x] = find(parent[x]);
+}
+
+void merge(int u, int v)
+{
+    int root1 = find(u);
+    int root2 = find(v);
+    if (root1 != root2)
     {
-        for (j = 0; j < g[i].size(); j++)
+        if (parent[root1] < parent[root2])
         {
-            edge tmp = g[i][j];
-            if (i < tmp.v)
-            {
-                edges[num++] = node(i, tmp.v, tmp.w);
-            }
+            parent[root1] += parent[root2];
+            parent[root2] = root1;
+        }
+        else
+        {
+            parent[root2] += parent[root1];
+            parent[root1] = root2;
         }
     }
-    sort(edges, edges + cnt);
-    for (i = 0; i < cnt; i++)
+}
+
+void kruskal(int n)
+{
+    sort(edges.begin(), edges.end());
+    int count = 0;
+    for (auto &e : edges)
     {
-        int u = edges[i].u, v = edges[i].v;
-        if (query(u) != query(v))
+        if (find(e.u) != find(e.v))
         {
-            merge(u, v);
-            t[u].push_back(v);
-            t[v].push_back(u);
-            if (++num == n - 1)
-            {
+            merge(e.u, e.v);
+            t[e.u].push_back(e.v);
+            t[e.v].push_back(e.u);
+            if (++count == n - 1)
                 break;
+        }
+    }
+}
+
+int main()
+{
+    edges.push_back(edge(1, 2, 6));
+    edges.push_back(edge(1, 3, 3));
+    edges.push_back(edge(2, 4, 1));
+    edges.push_back(edge(2, 6, 5));
+    edges.push_back(edge(3, 4, 6));
+    edges.push_back(edge(3, 5, 6));
+    edges.push_back(edge(4, 5, 6));
+    edges.push_back(edge(5, 6, 2));
+
+    kruskal(6);
+
+    for (int i = 1; i <= 6; i++)
+    {
+        for (int v : t[i])
+        {
+            if (i < v)
+            {
+                cout << i << " - " << v << endl;
             }
         }
     }
+
+    return 0;
 }
--- graph_2.5_P.cpp
+++ graph_2.5_P.cpp
@@ -3,7 +3,8 @@
 #include <queue>
 using namespace std;
 
-#define vNum 100
+#define vNum 6
+#define INF 9999
 
 struct edge
 {
@@ -21,18 +22,21 @@ struct node
         return w > n1.w;
     }
 };
-bool vis[vNum];
 
-void prim(vector<edge> g[vNum], int n, vector<int> t[vNum])
+bool vis[vNum + 1];
+vector<edge> g[vNum + 1];
+vector<int> t[vNum + 1];
+
+void prim(vector<edge> g[], int n, vector<int> t[])
 {
     priority_queue<node> pq;
-    int i, step = n - 1, u, v;
-    for (i = 0; i < g[1].size(); i++)
+    int step = n - 1, u, v;
+    for (int i = 0; i < g[1].size(); i++)
     {
         edge tmp = g[1][i];
         pq.push(node(1, tmp.v, tmp.w));
     }
-    vis[1] = 1;
+    vis[1] = true;
     while (step--)
     {
         node tmp = pq.top();
@@ -42,8 +46,9 @@ void prim(vector<edge> g[vNum], int n, vector<int> t[vNum])
             tmp = pq.top();
             pq.pop(), v = tmp.v;
         }
-        u = tmp.u, vis[tmp.v] = 1;
-        t[v].push_back(u), t[u].push_back(v);
+        u = tmp.u, vis[v] = true;
+        t[u].push_back(v);
+        t[v].push_back(u);
         for (int i = 0; i < g[v].size(); i++)
         {
             edge tmp = g[v][i];
@@ -53,4 +58,39 @@ void prim(vector<edge> g[vNum], int n, vector<int> t[vNum])
             }
         }
     }
+}
+
+int main()
+{
+    g[1].push_back(edge(2, 6));
+    g[1].push_back(edge(3, 3));
+    g[2].push_back(edge(1, 6));
+    g[2].push_back(edge(4, 1));
+    g[2].push_back(edge(6, 5));
+    g[3].push_back(edge(1, 3));
+    g[3].push_back(edge(4, 6));
+    g[3].push_back(edge(5, 6));
+    g[4].push_back(edge(2, 1));
+    g[4].push_back(edge(3, 6));
+    g[4].push_back(edge(5, 6));
+    g[5].push_back(edge(3, 6));
+    g[5].push_back(edge(4, 6));
+    g[5].push_back(edge(6, 2));
+    g[6].push_back(edge(2, 5));
+    g[6].push_back(edge(5, 2));
+
+    prim(g, 6, t);
+
+    for (int i = 1; i <= 6; i++)
+    {
+        for (int v : t[i])
+        {
+            if (i < v)
+            {
+                cout << i << " - " << v << endl;
+            }
+        }
+    }
+
+    return 0;
 }
